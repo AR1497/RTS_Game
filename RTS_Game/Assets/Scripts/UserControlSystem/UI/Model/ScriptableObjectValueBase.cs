572d919e75a1eacd7cfbec75c07da6d88e937258
AR1497 <vladimirova.a0501200@mail.ru>
2022-06-08 20:59:04 +0300

    Create Base Scriptable Object

---
+++ RTS_Game/Assets/Scripts/UserControlSystem/UI/Model/ScriptableObjectValueBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace UserControlSystem
+{
+    public sealed class ScriptableObjectValueBase<T> : ScriptableObject
+    {
+        public T CurrentValue { get; private set; }
+        public Action<T> Value;
+
+        public void SetValue(T value)
+        {
+            CurrentValue = value;
+            Value?.Invoke(value);
+        }
+    }
+}
