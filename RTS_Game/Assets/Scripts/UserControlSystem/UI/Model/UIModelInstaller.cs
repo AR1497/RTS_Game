66ec7931a16ea398d7a4a7324c322800cf0d65ca
AR1497 <vladimirova.a0501200@mail.ru>
2022-06-08 13:31:53 +0300

    Add Model UI

---
+++ RTS_Game/Assets/Scripts/UserControlSystem/UI/Model/UIModelInstaller.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel;
+using Abstractions.Commands.CommandsInterfaces;
+using UnityEngine;
+using Utils;
+using Zenject;
+
+namespace UserControlSystem
+{
+    public class UIModelInstaller : MonoInstaller
+    {
+        [SerializeField] private AssetsContext _legacyContext;
+        [SerializeField] private Vector3Value _vector3Value;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<AssetsContext>().FromInstance(_legacyContext);
+            Container.Bind<Vector3Value>().FromInstance(_vector3Value);
+
+            Container.Bind<CommandCreatorBase<IProduceUnitCommand>>()
+                .To<ProduceUnitCommandCreator>().AsTransient();
+            Container.Bind<CommandCreatorBase<IAttackCommand>>()
+                .To<AttackCommandCreator>().AsTransient();
+            Container.Bind<CommandCreatorBase<IMoveCommand>>()
+                .To<MoveCommandCreator>().AsTransient();
+            Container.Bind<CommandCreatorBase<IPatrolCommand>>()
+                .To<PatrolCommandCreator>().AsTransient();
+            Container.Bind<CommandCreatorBase<IHoldPositionCommand>>()
+                .To<HoldPositionCommandCreator>().AsTransient();
+
+            Container.Bind<CommandButtonsModel>().AsTransient();
+        }
+    }
+}
