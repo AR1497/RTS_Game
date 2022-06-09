687cbc2f3ece7a2ddc14f4afaa33269b82525460
AR1497 <vladimirova.a0501200@mail.ru>
2022-06-09 11:54:47 +0300

    Create Project Context

--- RTS_Game/Assets/Scripts/AssetInstaller.cs
+++ RTS_Game/Assets/Scripts/AssetInstaller.cs
@@ -8,15 +8,14 @@ using Zenject;
 [CreateAssetMenu(fileName = "AssetInstaller", menuName = "Installers/AssetInstaller")]
 public class AssetInstaller : ScriptableObjectInstaller<AssetInstaller>
 {
-    [SerializeField] private AssetsContext _legacyContext;
+    [SerializeField] private AssetsContext _assetContext;
     [SerializeField] private ScriptableObjectValueBase<Vector3> _vector3Value;
     [SerializeField] private ScriptableObjectValueBase<IAttackCommand> _atackCommand;
     [SerializeField] private ScriptableObjectValueBase<IPatrolCommand> _patrolCommand;
-    [SerializeField] private ScriptableObjectValueBase<IHoldPositionCommand> _holdPositionCommand;
     [SerializeField] private ScriptableObjectValueBase<ISelectable> _selectable;
 
     public override void InstallBindings()
     {
-        Container.BindInstances(_legacyContext, _vector3Value, _atackCommand, _patrolCommand, _holdPositionCommand, _selectable);
+        Container.BindInstances(_assetContext, _vector3Value, _atackCommand, _patrolCommand, _selectable);
     }
 }
