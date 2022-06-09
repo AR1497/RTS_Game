9baf1d919a3214d4f6e0d8c3b522b739f6ed0de7
AR1497 <vladimirova.a0501200@mail.ru>
2022-06-09 13:13:38 +0300

    Change Command

--- RTS_Game/Assets/Scripts/UserControlSystem/CommandsRealization/AttackUnitCommand.cs
+++ RTS_Game/Assets/Scripts/UserControlSystem/CommandsRealization/AttackUnitCommand.cs
@@ -4,6 +4,11 @@ namespace UserControlSystem.CommandsRealization
 {
     public sealed class AttackUnitCommand : IAttackCommand
     {
+        public AttackUnitCommand(IAttackable target)
+        {
+            Target = target;
+        }
 
+        public IAttackable Target { get; }
     }
 }
--- RTS_Game/Assets/Scripts/UserControlSystem/CommandsRealization/PatrolUnitCommand.cs
+++ RTS_Game/Assets/Scripts/UserControlSystem/CommandsRealization/PatrolUnitCommand.cs
@@ -1,9 +1,24 @@
 using Abstractions.Commands.CommandsInterfaces;
+using UnityEngine;
 
 namespace UserControlSystem.CommandsRealization
 {
     public class PatrolUnitCommand : IPatrolCommand
     {
-        
+        private readonly Vector3 _from;
+        private readonly Vector3 _to;
+
+        public PatrolUnitCommand(Vector3 from, Vector3 to)
+        {
+            _from = from;
+            _to = to;
+        }
+
+        public Vector3 FromPosition => _from;
+        public Vector3 ToPosition => _to;
+        public void Patrol(Vector3 from, Vector3 to)
+        {
+            Debug.Log($"Patrolling from {from} to {to}");
+        }
     }
 }
