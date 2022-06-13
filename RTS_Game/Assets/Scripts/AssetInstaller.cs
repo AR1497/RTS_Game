0ce8a50434411546d9050ab88cea0ae177258b99
AR1497 <vladimirova.a0501200@mail.ru>
2022-06-13 22:17:43 +0300

    Asset Installer

--- RTS_Game/Assets/Scripts/AssetInstaller.cs
+++ RTS_Game/Assets/Scripts/AssetInstaller.cs
@@ -10,7 +10,7 @@ public class AssetInstaller : ScriptableObjectInstaller<AssetInstaller>
 {
     [SerializeField] private AssetsContext _assetContext;
     [SerializeField] private ScriptableObjectValueBase<Vector3> _vector3Value;
-    [SerializeField] private ScriptableObjectValueBase<IAttackCommand> _atackCommand;
+    [SerializeField] private ScriptableObjectValueBase<IAttackable> _atackCommand;
     [SerializeField] private ScriptableObjectValueBase<IPatrolCommand> _patrolCommand;
     [SerializeField] private ScriptableObjectValueBase<ISelectable> _selectable;
 
