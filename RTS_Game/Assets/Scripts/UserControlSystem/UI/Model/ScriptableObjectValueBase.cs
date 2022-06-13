c8818d7bcb389b911d8993f7307240470dc12a66
AR1497 <vladimirova.a0501200@mail.ru>
2022-06-13 22:23:18 +0300

    Edit Base selectable object value

--- RTS_Game/Assets/Scripts/UserControlSystem/UI/Model/ScriptableObjectValueBase.cs
+++ RTS_Game/Assets/Scripts/UserControlSystem/UI/Model/ScriptableObjectValueBase.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using UnityEngine;
+using Utils;
 
 namespace UserControlSystem
 {
-    public class ScriptableObjectValueBase<T> : ScriptableObject
+    public class ScriptableObjectValueBase<T> : ScriptableObject, IAwaitable<T>
     {
+        public class NewValueNotifier<TAwaited> : IAwaiter<TAwaited>
+        {
+            private readonly ScriptableObjectValueBase<TAwaited> _scriptableObjectValueBase;
+            private TAwaited _result;
+            private Action _continuation;
+            private bool _isCompleted;
+
+            public NewValueNotifier(ScriptableObjectValueBase<TAwaited> scriptableObjectValueBase)
+            {
+                _scriptableObjectValueBase = scriptableObjectValueBase;
+                _scriptableObjectValueBase.OnNewValue += ONNewValue;
+            }
+
+            private void ONNewValue(TAwaited obj)
+            {
+                _scriptableObjectValueBase.OnNewValue -= ONNewValue;
+                _result = obj;
+                _isCompleted = true;
+                _continuation?.Invoke();
+            }
+
+            public void OnCompleted(Action continuation)
+            {
+                if (_isCompleted)
+                {
+                    continuation?.Invoke();
+                }
+                else
+                {
+                    _continuation = continuation;
+                }
+            }
+            public bool IsCompleted => _isCompleted;
+            public TAwaited GetResult() => _result;
+        }
+        
         public T CurrentValue { get; private set; }
         public Action<T> OnNewValue;
 
@@ -12,6 +49,11 @@ namespace UserControlSystem
         {
             CurrentValue = value;
             OnNewValue?.Invoke(value);
+        }
+
+        public IAwaiter<T> GetAwaiter()
+        {
+            return new NewValueNotifier<T>(this);
         }
     }
 }
