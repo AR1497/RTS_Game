9c145055c0f10b9133dd890b9a2ce55a9204dfcb
AR1497 <vladimirova.a0501200@mail.ru>
2022-06-15 13:04:01 +0300

    Realised Hold Position Command

--- RTS_Game/Assets/Scripts/Abstractions/Commands/CommandExecutors/HoldPositionCommandExecutor.cs
+++ RTS_Game/Assets/Scripts/Abstractions/Commands/CommandExecutors/HoldPositionCommandExecutor.cs
@@ -1,11 +1,12 @@
 using Abstractions.Commands;
 using Abstractions.Commands.CommandsInterfaces;
-using UnityEngine;
+using System.Threading;
 
 public class HoldPositionCommandExecutor : CommandExecutorBase<IHoldPositionCommand>
 {
+    public CancellationTokenSource CancellationTokenSource { get; set; }
     public override void ExecuteSpecificCommand(IHoldPositionCommand command)
     {
-        Debug.Log($"Stoping {gameObject.name}");
+        CancellationTokenSource?.Cancel();
     }
 }
