c40ae9b57f6c6a7f3e37e379ae5ae18e8dc266a1
AR1497 <vladimirova.a0501200@mail.ru>
2022-06-20 11:00:41 +0300

    Add _selectedValue

--- RTS_Game/Assets/Scripts/AssetInstaller.cs
+++ RTS_Game/Assets/Scripts/AssetInstaller.cs
@@ -1,5 +1,6 @@
 using Abstractions;
 using Abstractions.Commands.CommandsInterfaces;
+using System;
 using UnityEngine;
 using UserControlSystem;
 using Utils;
@@ -9,15 +10,17 @@ using Zenject;
 public class AssetInstaller : ScriptableObjectInstaller<AssetInstaller>
 {
     [SerializeField] private AssetsContext _assetContext;
-    [SerializeField] private ScriptableObjectValueBase<Vector3> _vector3Value;
-    [SerializeField] private ScriptableObjectValueBase<IAttackable> _atackCommand;
+    [SerializeField] private StatelessScriptableObjectValueBase<Vector3> _vector3Value;
+    [SerializeField] private StatelessScriptableObjectValueBase<IAttackable> _atackCommand;
     [SerializeField] private ScriptableObjectValueBase<IPatrolCommand> _patrolCommand;
-    [SerializeField] private ScriptableObjectValueBase<ISelectable> _selectable;
+    [SerializeField] private StatefulScriptableObjectValueBase<ISelectable> _selectable;
+    [SerializeField] private IObservable<ISelectable> _selectedValues;
 
     public override void InstallBindings()
     {
         Container.BindInstances(_assetContext, _vector3Value, _atackCommand, _patrolCommand, _selectable);
         Container.Bind<IAwaitable<IAttackable>>().FromInstance(_atackCommand);
         Container.Bind<IAwaitable<Vector3>>().FromInstance(_vector3Value);
+        Container.Bind<IObservable<ISelectable>>().FromInstance(_selectedValues);
     }
 }
