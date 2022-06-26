dbe30fbf194b8cdcfc23871822f6ab3b43985bfe
AR1497 <vladimirova.a0501200@mail.ru>
2022-06-26 16:04:49 +0300

    Edit base class

--- RTS_Game/Assets/Scripts/Utils/ScriptableObjectValueBase.cs
+++ RTS_Game/Assets/Scripts/Utils/ScriptableObjectValueBase.cs
@@ -4,44 +4,8 @@ using Utils;
 
 namespace UserControlSystem
 {
-    public class ScriptableObjectValueBase<T> : ScriptableObject, IAwaitable<T>
+    public partial class ScriptableObjectValueBase<T> : ScriptableObject, IAwaitable<T>
     {
-        public class NewValueNotifier<TAwaited> : AwaiterBase<TAwaited>
-        {
-            private readonly ScriptableObjectValueBase<TAwaited> _scriptableObjectValueBase;
-            private TAwaited _result;
-            private Action _continuation;
-            private bool _isCompleted;
-
-            public NewValueNotifier(ScriptableObjectValueBase<TAwaited> scriptableObjectValueBase)
-            {
-                _scriptableObjectValueBase = scriptableObjectValueBase;
-                _scriptableObjectValueBase.OnNewValue += ONNewValue;
-            }
-
-            private void ONNewValue(TAwaited obj)
-            {
-                _scriptableObjectValueBase.OnNewValue -= ONNewValue;
-                _result = obj;
-                _isCompleted = true;
-                _continuation?.Invoke();
-            }
-
-            public void OnCompleted(Action continuation)
-            {
-                if (_isCompleted)
-                {
-                    continuation?.Invoke();
-                }
-                else
-                {
-                    _continuation = continuation;
-                }
-            }
-            public bool IsCompleted => _isCompleted;
-            public TAwaited GetResult() => _result;
-        }
-        
         public T CurrentValue { get; private set; }
         public Action<T> OnNewValue;
 
