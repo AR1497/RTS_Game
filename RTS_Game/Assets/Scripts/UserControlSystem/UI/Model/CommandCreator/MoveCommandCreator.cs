ad2d8389773a4e8a65da9f081c6ab6467d6b3a91
AR1497 <vladimirova.a0501200@mail.ru>
2022-06-28 19:57:36 +0300

    Edit creator

--- RTS_Game/Assets/Scripts/UserControlSystem/UI/Model/CommandCreator/MoveCommandCreator.cs
+++ RTS_Game/Assets/Scripts/UserControlSystem/UI/Model/CommandCreator/MoveCommandCreator.cs
@@ -9,27 +9,6 @@ namespace UserControlSystem
 {
     public sealed class MoveCommandCreator : CancellableCommandCreatorBase<IMoveCommand, Vector3>
     {
-        [Inject] private AssetsContext _context;
-        private Action<IMoveCommand> _creationCallback;
-
         protected override IMoveCommand CreateCommand(Vector3 argument) => new MoveUnitCommand(argument);
-
-        [Inject]
-        private void Init(ScriptableObjectValueBase<Vector3> groundClicks) => groundClicks.OnNewValue += ONNewValue;
-
-        private void ONNewValue(Vector3 groundClick)
-        {
-            _creationCallback?.Invoke(_context.Inject(new MoveUnitCommand(groundClick)));
-            _creationCallback = null;
-        }
-
-        protected override void ClassSpecificCommandCreation(Action<IMoveCommand> creationCallback)
-            => _creationCallback = creationCallback;
-
-        public override void ProcessCancel()
-        {
-            base.ProcessCancel();
-            _creationCallback = null;
-        }
     }
 }
--- RTS_Game/Assets/Scripts/UserControlSystem/UI/Model/CommandCreator/PatrolCommandCreator.cs
+++ RTS_Game/Assets/Scripts/UserControlSystem/UI/Model/CommandCreator/PatrolCommandCreator.cs
@@ -1,38 +1,15 @@
-using System;
-using Abstractions;
 using Abstractions.Commands.CommandsInterfaces;
 using UnityEngine;
 using UserControlSystem.CommandsRealization;
-using Utils;
 using Zenject;
 
 namespace UserControlSystem
 {
-    public class PatrolCommandCreator : CancellableCommandCreatorBase<IPatrolCommand, Vector3>
+    public sealed class PatrolCommandCreator : CancellableCommandCreatorBase<IPatrolCommand, Vector3>
     {
-        [Inject] private AssetsContext _context;
-        [Inject] private SelectableValue _selectedObject;
+        [Inject] private SelectableValue _selectable;
 
-        private Action<IPatrolCommand> _creationCallback;
-
-        protected override IPatrolCommand CreateCommand(Vector3 argument)
-            => new PatrolUnitCommand(_selectedObject.CurrentValue.PivotPoint.position, argument);
-
-        [Inject]
-        private void Init(ScriptableObjectValueBase<Vector3> groundClicks)
-        {
-            groundClicks.OnNewValue += onNewValue;
-        }
-
-        private void onNewValue(Vector3 groundClick)
-        {
-            _creationCallback?.Invoke(_context.Inject(new PatrolUnitCommand(_selectedObject.CurrentValue.CurrenntPosition, groundClick)));
-            _creationCallback = null;
-        }
-
-        protected override void ClassSpecificCommandCreation(Action<IPatrolCommand> creationCallback)
-        {
-            _creationCallback = creationCallback;
-        }
+        protected override IPatrolCommand CreateCommand(Vector3 argument) 
+            => new PatrolUnitCommand(_selectable.CurrentValue.PivotPoint.position, argument);
     }
 }
