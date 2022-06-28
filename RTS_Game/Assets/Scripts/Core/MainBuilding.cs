36f080e9c654737a3d85586d9e05e759ad62a4f4
AR1497 <vladimirova.a0501200@mail.ru>
2022-06-28 19:56:16 +0300

    Edit main building

--- RTS_Game/Assets/Scripts/Core/MainBuilding.cs
+++ RTS_Game/Assets/Scripts/Core/MainBuilding.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-    public class MainBuilding : CommandExecutorBase<IProduceUnitCommand>, ISelectable
+    public class MainBuilding : MonoBehaviour, ISelectable
     {
         public float Health => _health;
         public float MaxHealth => _maxHealth;
@@ -23,33 +23,6 @@ namespace Core
         private float _health = 1000;
 
         public Vector3 CurrenntPosition => gameObject.transform.position;
-
-        public override async Task ExecuteSpecificCommand(IProduceUnitCommand command)
-        {
-            await CreateUnitTask(command);
-        }
-
-        private async Task CreateUnitTask(IProduceUnitCommand command)
-        {
-            await Task.Delay(5000);
-            Instantiate(command.UnitPrefab,
-                                   new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)),
-                                   Quaternion.identity,
-                                   _unitsParent);
-        }
-
-        public void RecieveDamage(int amount)
-        {
-            if (_health <= 0)
-            {
-                return;
-            }
-            _health -= amount;
-            if (_health <= 0)
-            {
-                Destroy(gameObject);
-            }
-        }
 
         public void UnsetSelected()
         {
