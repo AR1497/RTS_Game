8cacad20de08b3cc8b414f92ee3367bae947ca06
AR1497 <vladimirova.a0501200@mail.ru>
2022-07-01 13:50:41 +0300

    Edit Command

--- RTS_Game/Assets/Scripts/Core/HoldPositionCommand.cs
+++ RTS_Game/Assets/Scripts/Core/HoldPositionCommand.cs
@@ -1,9 +1,10 @@
 using Abstractions.Commands.CommandsInterfaces;
+using UnityEngine;
 
 namespace Core
 {
     public class HoldPositionCommand : IHoldPositionCommand
     {
-
+        public Vector3 Target => throw new System.NotImplementedException();
     }
 }
