b0230934b403b23d58f8d09d0a8309dc229b8ae0
AR1497 <vladimirova.a0501200@mail.ru>
2022-07-01 13:51:48 +0300

    Takeaway

--- RTS_Game/Assets/Scripts/Core/TimeModel.cs
+++ RTS_Game/Assets/Scripts/Core/TimeModel.cs
@@ -9,7 +9,8 @@ namespace Core
     public sealed class TimeModel : ITimeModel, ITickable
     {
         public IObservable<int> GameTime => _gameTime.Select(f => (int)f);
-        private readonly ReactiveProperty<float> _gameTime = new ReactiveProperty<float>();
+        private ReactiveProperty<float> _gameTime = new
+            ReactiveProperty<float>();
 
         public void Tick()
         {
