1cb11f6a2a32ac2f011af9b5401f873d65e21118
AR1497 <vladimirova.a0501200@mail.ru>
2022-07-06 09:13:08 +0300

    Edit main building

--- RTS_Game/Assets/Scripts/Core/MainBuilding.cs
+++ RTS_Game/Assets/Scripts/Core/MainBuilding.cs
@@ -6,13 +6,12 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-    public class MainBuilding : CommandExecutorBase<IProduceUnitCommand>, ISelectable
+    public class MainBuilding : MonoBehaviour, ISelectable /*CommandExecutorBase<IProduceUnitCommand>, ISelectable*/
     {
         public float Health => _health;
         public float MaxHealth => _maxHealth;
-        public Transform PivotPoint => throw new System.NotImplementedException();
+        public Transform PivotPoint => _unitsParent;
         public Sprite Icon => _icon;
-        public Vector3 CurrenntPosition => gameObject.transform.position;
 
         public Vector3 RallyPoint { get; set; }
 
@@ -23,16 +22,16 @@ namespace Core
 
         private float _health = 1000;
 
-        public override async Task ExecuteSpecificCommand(IProduceUnitCommand command)
-        {
-            await CreateUnitTask(command);
-        }
+        //public override async Task ExecuteSpecificCommand(IProduceUnitCommand command)
+        //{
+        //    await CreateUnitTask(command);
+        //}
 
-        private async Task CreateUnitTask(IProduceUnitCommand command)
-        {
-            await Task.Delay(2000);
-            Instantiate(command.UnitPrefab, new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)), Quaternion.identity, _unitsParent);
-        }
+        //private async Task CreateUnitTask(IProduceUnitCommand command)
+        //{
+        //    await Task.Delay(2000);
+        //    Instantiate(command.UnitPrefab, new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)), Quaternion.identity, _unitsParent);
+        //}
 
         public void UnsetSelected()
         {
